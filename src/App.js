a59e4adc5edc4c327e03309a23e213bead6d1265
adarsh1997shah <adarshshah01@gmail.com>
2021-02-01 22:17:49 +0530

    added lazy load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { LoginContext } from './components/context/login.context';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
-import Home from './pages/Home';
-import Login from './pages/Login';
+
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+
+const loader = {
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <LoginContext>
-          <Switch>
-            <PublicRoute path="/login">
-              <Login />
-            </PublicRoute>
+          <Suspense fallback={<div style={loader}>Loading...</div>}>
+            <Switch>
+              <PublicRoute path="/login">
+                <Login />
+              </PublicRoute>
 
-            <PrivateRoute path="/">
-              <Home />
-            </PrivateRoute>
-          </Switch>
+              <PrivateRoute path="/">
+                <Home />
+              </PrivateRoute>
+            </Switch>
+          </Suspense>
         </LoginContext>
       </BrowserRouter>
     </div>
